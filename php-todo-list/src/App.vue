31c73ef1077508cb687742f2be034f59c7115bfa
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-06-22 15:11:22 +0100

    first commit

---
+++ php-todo-list/src/App.vue
@@ -0,0 +1,54 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+
+    return {
+      todoitems: [],
+
+      // newStudent: {
+      //   name: "",
+      //   last_name: ""
+      // }
+    };
+  },
+  mounted() {
+    axios.get('http://localhost/Booleans-php/php-todo-list-json/php/index.php')
+      .then(response => {
+        this.todoitems = response.data;
+        console.log(response.data);
+      });
+  }
+};
+
+
+
+</script>
+
+<template>
+  <h1>
+    Todo list
+  </h1>
+  <div class="container">
+    <ul>
+      <li v-for="(item, index) in todoitems" :key="index">
+        {{ item.name }}
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.container {
+  width: 40%;
+  height: 500px;
+  margin: auto;
+  background-color: blueviolet;
+}
+</style>
---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5175");
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
+
+$dataStr = file_get_contents('data.json');
+echo $datatStr;
+
+var_dump($dataStr);
